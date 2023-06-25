12ce436d27ac90dd4adb3cb6046b357d2e588dd2
mario <r0895582@student.vives.be>
2023-06-25 17:33:27 +0200

    add: responsive design landing

--- src/app/homepage/hero/hero.component.ts
+++ src/app/homepage/hero/hero.component.ts
@@ -4,8 +4,24 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
   faArrowRight = faArrowRight;
+  heroText: HTMLElement = document.querySelector('#hero-text');
+
+  constructor() {}
+
+  ngOnInit(): void {
+    window.addEventListener('resize', this.updateHeroTextClass.bind(this));
+    this.updateHeroTextClass();
+  }
+
+  updateHeroTextClass() {
+    if (window.innerWidth <= 1024) {
+      this.heroText.classList.add('col-12');
+    } else {
+      this.heroText.classList.remove('col-7');
+    }
+  }
 }
