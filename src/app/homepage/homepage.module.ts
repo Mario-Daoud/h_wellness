8ff2da85c220de38e1c8b38b15c990de520cfde3
mario <r0895582@student.vives.be>
2023-06-25 21:29:57 +0200

    add: about page

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
-  declarations: [HeroComponent],
+  declarations: [HeroComponent, AboutusComponent],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [HeroComponent],
+  exports: [HeroComponent, AboutusComponent],
 })
 export class HomepageModule {}
