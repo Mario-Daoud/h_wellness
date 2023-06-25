c464b4dfa44f042e79026b38eb7af04c700b4392
mario <r0895582@student.vives.be>
2023-06-25 21:32:32 +0200

    create service componen

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { OurservicesComponent } from './ourservices/ourservices.component';
 
 @NgModule({
-  declarations: [HeroComponent, AboutusComponent],
+  declarations: [HeroComponent, AboutusComponent, OurservicesComponent],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [HeroComponent, AboutusComponent],
+  exports: [HeroComponent, AboutusComponent, OurservicesComponent],
 })
 export class HomepageModule {}
